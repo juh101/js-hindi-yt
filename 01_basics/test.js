f986194be8bc5ed9094b01f270f0262540872e67
juh101 <juhiahuja2006@gmail.com>
2025-06-04 10:11:53 +0000

    container config done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,17 @@
 console.log("hello")
+
+const accountId=222
+let accEmail="juhi3001@gmail.com"
+var accPw="juhi69"
+accountCity="jaipur" //not the right method,either use const or let
+let accState; //holds value"undefined" value until given one
+
+// accountId=54 this will throw an error
+// {
+//     this is scope
+//      var does not have block scope that is why we use let
+//      use const and let only
+// }
+console.log(accountId)
+console.log((typeof(accountCity)))
+console.table([accountId , accEmail , accPw , accountCity , accState])
