ce645de4ad0cc825c410305ddd96d39aaca78cd9
juh101 <juhiahuja2006@gmail.com>
2025-06-08 08:51:31 +0000

    memory basics

--- 01_basics/05_memory.js
+++ 01_basics/05_memory.js
@@ -26,3 +26,15 @@ user2.email = "second@gmail.com"
 
 console.log(user1.email)
 console.log(user2.email)
+/*
+🧹 What is GC (Garbage Collection) in Programming?
+GC stands for Garbage Collection — it's an automatic process used in 
+programming languages like JavaScript, Java, Python, etc., to manage memory.
+When you create variables or objects in your program, memory is used.
+
+But when you no longer need that data, the memory should be freed.
+In garbage-collected languages, you don’t have to do this manually.
+
+Instead, the Garbage Collector (GC) does it for you.
+It finds the memory that’s no longer in use and clears it.
+*/
