75fcbd2488f76901fd703b9ab526365204b67cfa
juh101 <juhiahuja2006@gmail.com>
2025-06-09 07:40:58 +0000

    math and num

--- 01_basics/07_nums_maths.js
+++ 01_basics/07_nums_maths.js
@@ -26,6 +26,12 @@
 // console.log(Math.max(0.1 , 3 , 6 ,99));
 // console.log(Math.min(0.1 , 3 , 6 ,99));
 
-console.log(Math.floor(10*(Math.random())));
+console.log((Math.random())); //always returns value between 0-1
+console.log(Math.floor(Math.random()*10) + 1); //will return value between 0-10
+
+const min = 10
+const max = 20
+
+console.log((Math.floor(Math.random() * (max - min +1)) + min))
 
 
